33213eb05f2ec585cf2a6d3bfa9552861628e000
Unknown <enevik@hotmail.com>
2019-10-08 13:50:03 +0200

    volume slider werkt

---
+++ Asteroids/Assets/Script/VolumeChange.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeChange : MonoBehaviour
+{
+    public Slider volumeSlider;
+    public AudioSource volumeAudio;
+    public void VolumeController()
+    {
+        volumeAudio.volume = volumeSlider.value;
+    }
+}
