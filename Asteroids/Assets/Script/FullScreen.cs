8df4944d983d8b39f27edfc27149cda9aa26d082
Unknown <enevik@hotmail.com>
2019-10-22 14:41:02 +0200

    fullscreen

---
+++ Asteroids/Assets/Script/FullScreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullScreen : MonoBehaviour
+{
+    public void fullScreen()
+    {
+        // Switch to 640 x 480 full-screen
+        Screen.SetResolution(640, 480, true);
+    }
+}
